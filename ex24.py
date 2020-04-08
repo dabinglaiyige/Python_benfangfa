3db78298ea1b6b8a1b68fc0fa016362887a57797
dabinglaiyige <jasonsee706@gmail.com>
2020-04-08 21:33:15 +0800

    add 24

---
+++ ex24.py
@@ -0,0 +1,14 @@
+print("Let's practice everything.")
+print('You\' need to know \'bout escapes with \\ that do:')
+print('\n newlines and \t tabs')
+
+poem = """
+\tThe lovely world
+with logic so firmly planted
+connot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is gone.
+"""
+
+print("--------------")
